d72e2a53b4b542ad8adf1807d7b94a7788ab00cf
Sean T. Allen <sean@seantallen.com>
2020-05-09 22:00:43 -0400

    Fix bad versioning from sed

--- ponylang-mode.el
+++ ponylang-mode.el
@@ -1,7 +1,7 @@
 ;;; ponylang-mode.el --- Major mode for Pony code
 ;;
 ;; Authors: Sean T Allen <sean@monkeysnatchbanana.com>
-;; Version: ${VERSION}
+;; Version: 0.0.12
 ;; URL: https://github.com/seantallen/ponylang-mode
 ;; Keywords: languages programming
 ;; Package-Requires: ((dash "2.10.0"))
