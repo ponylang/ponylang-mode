c592577cdf85cd9a5bc1d7c2fd2c878f4472526d
Damon Kwok <damon-kwok@outlook.com>
2020-06-01 11:41:16 +0800

    Fixed generic references highlighting (#86)

--- ponylang-mode.el
+++ ponylang-mode.el
@@ -300,7 +300,7 @@ by parse-partial-sexp, and should return a face. "
     (,ponylang-constant-regexp . font-lock-constant-face)
 
     ;; type references: second filter
-    ("\\(\s\\|[\[]\\|[\(]\\)\\($?_?[A-Z][A-Za-z0-9_]*\\)" 2 'font-lock-type-face)
+    ("\\(\s\\|->\\|[\[]\\|[\(]\\)\\($?_?[A-Z][A-Za-z0-9_]*\\)" 2 'font-lock-type-face)
 
     ;; tuple references
     ("[.]$?[ \t]?\\($?_[1-9]$?[0-9]?*\\)" 1 'font-lock-variable-name-face)
