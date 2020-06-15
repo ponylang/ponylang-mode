cd61df4c0222d0b21d6c915c75def8671887d96a
damon-kwok <damon-kwok@outlook.com>
2020-06-15 17:59:11 +0800

    Update share region menu

--- ponylang-mode.el
+++ ponylang-mode.el
@@ -598,6 +598,11 @@ the current context."
         (length selection))
     0))
 
+(defun ponylang-share-region-auto ()
+  "Create a shareable URL for the region on the Pony `playground'."
+  (if (> (ponylang-region-length) 0)
+      (ponylang-share-region (region-beginning) (region-end))))
+
 (defun ponylang-share-buffer ()
   "Create a shareable URL for the contents of the buffer on the Pony `playground'."
   (interactive)
@@ -610,8 +615,7 @@ the current context."
      "---"
      ("Playground";
        ["Share Buffer"          ponylang-share-buffer t]
-       ["Share Region"          (ponylang-share-region (region-beginning) (region-end))
-         (> (ponylang-region-length) 0)])
+       ["Share Region"          ponylang-share-region-auto (use-region-p)])
      "---"                               ;
      ("Corral" ["Init" ponylang-corral-init t]
        ["Open" ponylang-corral-open t]
